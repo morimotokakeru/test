ee838a97523a2cd885d96e397426fd041ff133a1
wiyoungpark <wiyoungpark@10.211.55.4>
2018-06-08 14:22:25 +0900

    dao:敬称プルダウン、ワンレコード取得 updateJSP、FORM、Action追加 update画面遷移

---
+++ TEST5/WebContent/WEB-INF/views/update.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="java.util.List"%>
+<%@ page import="beans.UserBean"%>
+<%@ taglib uri="http://struts.apache.org/tags-html" prefix="html"%>
+<%@ taglib uri="http://struts.apache.org/tags-logic" prefix="logic" %>
+<%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<html:form action="/users/update" method="POST">
+	<html:select property="title">
+		<logic:iterate id="list" name="titleList">
+			<html:option value="${list.titleName }"/>
+		</logic:iterate>
+	</html:select>
+	</html:form>
+</body>
+</html>
--- TEST5/src/dao/UserDao.java
+++ TEST5/src/dao/UserDao.java
@@ -8,11 +8,13 @@ import java.util.List;
 
 import DBconnector.DBConnector;
 import beans.UserBean;
+import users.UpdateForm;
 import users.UserViewForm;
 
-public class UserDao{
+public class UserDao {
 
-	public List<UserBean> getUserAll(UserViewForm form){
+	//検索・全レコード取り出し
+	public List<UserBean> getUserAll(UserViewForm form) {
 		DBConnector db = new DBConnector();
 		List<UserBean> users = null;
 		PreparedStatement st = null; // SQLを送るとき必要 /クラス型のst
@@ -21,47 +23,47 @@ public class UserDao{
 			String sql = "SELECT * FROM CUSTOMER WHERE 1=1";// 準備//
 
 			StringBuilder Sql = new StringBuilder(sql);
-			if(form.getUserId() != 0) {
+			if (form.getUserId() != 0) {
 				Sql.append(" and USER_ID = ?");
 			}
-			if(form.getFirstName() != null && !form.getFirstName().isEmpty()) {
+			if (form.getFirstName() != null && !form.getFirstName().isEmpty()) {
 				Sql.append(" and FIRST_NAME like ?");
 			}
-			if(form.getLastName() != null && !form.getLastName().isEmpty()) {
+			if (form.getLastName() != null && !form.getLastName().isEmpty()) {
 				Sql.append(" and LAST_NAME like ?");
 			}
-			if(form.getFirstNameKana() != null && !form.getFirstNameKana().isEmpty()) {
+			if (form.getFirstNameKana() != null && !form.getFirstNameKana().isEmpty()) {
 				Sql.append(" and FIRST_NAME_KANA like ?");
 			}
-			if(form.getFirstNameKana() != null && !form.getFirstNameKana().isEmpty()) {
+			if (form.getFirstNameKana() != null && !form.getFirstNameKana().isEmpty()) {
 				Sql.append(" and LAST_NAME_KANA like ?");
 			}
-			if(form.getSex() != null && !form.getSex().isEmpty()) {
+			if (form.getSex() != null && !form.getSex().isEmpty()) {
 				Sql.append(" and SEX like ?");
 			}
-			if(form.getClassification1() != null && !form.getClassification1().isEmpty()) {
+			if (form.getClassification1() != null && !form.getClassification1().isEmpty()) {
 				Sql.append(" and CLASSIFICATION1 like ?");
 			}
-			if(form.getClassification2() != null && !form.getClassification2().isEmpty()) {
+			if (form.getClassification2() != null && !form.getClassification2().isEmpty()) {
 				Sql.append(" and CLASSIFICATION2 like ?");
 			}
-			if(form.getPositionName() != null && !form.getPositionName().isEmpty()) {
+			if (form.getPositionName() != null && !form.getPositionName().isEmpty()) {
 				Sql.append(" and POSITION_NAME like ?");
 			}
-			if(form.getStreet1() != null && !form.getStreet1().isEmpty()) {
+			if (form.getStreet1() != null && !form.getStreet1().isEmpty()) {
 				Sql.append(" and STREET1 like ?");
 			}
-			if(form.getTell() != null && !form.getTell().isEmpty()) {
+			if (form.getTell() != null && !form.getTell().isEmpty()) {
 				Sql.append(" and TELL like ?");
 			}
-			if(form.getFax() != null && !form.getFax().isEmpty()) {
+			if (form.getFax() != null && !form.getFax().isEmpty()) {
 				Sql.append(" and FAX like ?");
 
 			}
-			if(form.getMobile() != null && !form.getMobile().isEmpty()) {
+			if (form.getMobile() != null && !form.getMobile().isEmpty()) {
 				Sql.append(" and mobile like ?");
 			}
-			if(form.getEmail() != null && !form.getEmail().isEmpty()) {
+			if (form.getEmail() != null && !form.getEmail().isEmpty()) {
 				Sql.append(" and EMAIL like ?");
 			}
 
@@ -70,47 +72,47 @@ public class UserDao{
 
 			int index = 0;
 
-			if(form.getUserId() != 0) {
-				st.setInt(++index,form.getUserId());
+			if (form.getUserId() != 0) {
+				st.setInt(++index, form.getUserId());
 			}
-			if(form.getFirstName() != null && !form.getFirstName().isEmpty()) {
-				st.setString(++index,"%" + form.getFirstName() + "%");
+			if (form.getFirstName() != null && !form.getFirstName().isEmpty()) {
+				st.setString(++index, "%" + form.getFirstName() + "%");
 			}
-			if(form.getLastName() != null && !form.getLastName().isEmpty()) {
-				st.setString(++index,"%" + form.getLastName() + "%");
+			if (form.getLastName() != null && !form.getLastName().isEmpty()) {
+				st.setString(++index, "%" + form.getLastName() + "%");
 			}
-			if(form.getFirstNameKana() != null && !form.getFirstNameKana().isEmpty()) {
-				st.setString(++index,"%" + form.getFirstNameKana() + "%");
+			if (form.getFirstNameKana() != null && !form.getFirstNameKana().isEmpty()) {
+				st.setString(++index, "%" + form.getFirstNameKana() + "%");
 			}
-			if(form.getFirstNameKana() != null && !form.getFirstNameKana().isEmpty()) {
-				st.setString(++index,"%" + form.getFirstNameKana() + "%");
+			if (form.getFirstNameKana() != null && !form.getFirstNameKana().isEmpty()) {
+				st.setString(++index, "%" + form.getFirstNameKana() + "%");
 			}
-			if(form.getSex() != null && !form.getSex().isEmpty()) {
-				st.setString(++index,"%" + form.getSex() + "%");
+			if (form.getSex() != null && !form.getSex().isEmpty()) {
+				st.setString(++index, "%" + form.getSex() + "%");
 			}
-			if(form.getClassification1() != null && !form.getClassification1().isEmpty()) {
-				st.setString(++index,"%" + form.getClassification1() + "%");
+			if (form.getClassification1() != null && !form.getClassification1().isEmpty()) {
+				st.setString(++index, "%" + form.getClassification1() + "%");
 			}
-			if(form.getClassification2() != null && !form.getClassification2().isEmpty()) {
-				st.setString(++index,"%" + form.getClassification2() + "%");
+			if (form.getClassification2() != null && !form.getClassification2().isEmpty()) {
+				st.setString(++index, "%" + form.getClassification2() + "%");
 			}
-			if(form.getPositionName() != null && !form.getPositionName().isEmpty()) {
-				st.setString(++index,"%" + form.getPositionName() + "%");
+			if (form.getPositionName() != null && !form.getPositionName().isEmpty()) {
+				st.setString(++index, "%" + form.getPositionName() + "%");
 			}
-			if(form.getStreet1() != null && !form.getStreet1().isEmpty()) {
-				st.setString(++index,"%" + form.getStreet1() + "%");
+			if (form.getStreet1() != null && !form.getStreet1().isEmpty()) {
+				st.setString(++index, "%" + form.getStreet1() + "%");
 			}
-			if(form.getTell() != null && !form.getTell().isEmpty()) {
-				st.setString(++index,"%" + form.getTell() + "%");
+			if (form.getTell() != null && !form.getTell().isEmpty()) {
+				st.setString(++index, "%" + form.getTell() + "%");
 			}
-			if(form.getFax() != null && !form.getFax().isEmpty()) {
-				st.setString(++index,"%" + form.getFax() + "%");
+			if (form.getFax() != null && !form.getFax().isEmpty()) {
+				st.setString(++index, "%" + form.getFax() + "%");
 			}
-			if(form.getMobile() != null && !form.getMobile().isEmpty()) {
-				st.setString(++index,"%" + form.getMobile() + "%");
+			if (form.getMobile() != null && !form.getMobile().isEmpty()) {
+				st.setString(++index, "%" + form.getMobile() + "%");
 			}
-			if(form.getEmail() != null && !form.getEmail().isEmpty()) {
-				st.setString(++index,"%" + form.getEmail() + "%");
+			if (form.getEmail() != null && !form.getEmail().isEmpty()) {
+				st.setString(++index, "%" + form.getEmail() + "%");
 			}
 
 			rs = st.executeQuery();// 実行と結果の戻り//
@@ -134,9 +136,7 @@ public class UserDao{
 				user.setTell(rs.getString("TELL"));
 				user.setMobile(rs.getString("MOBILE"));
 				user.setEmail(rs.getString("EMAIL"));
-
 				users.add(user);
-				System.out.println(users);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -147,5 +147,108 @@ public class UserDao{
 
 	}
 
+	//Update
+	public void doUpdate(UpdateForm form) {
+		DBConnector db = new DBConnector();
+		PreparedStatement st = null; // SQLを送るとき必要 /クラス型のst
+		try {
+			String sql = "UPDATE CUSTOMER SET FIRST_NAME = ?, LAST_NAME = ?,"
+					+ "FIRST_NAME_KANA = ?, LAST_NAME_KANA = ?, TITLE = ?, SEX = ?, "
+					+ "CLASSIFICATION1 = ?, CLASSIFICATION2 = ?, POSITIO_NAME = ?, COMPANY = ?,"
+					+ " DEPARTMENT1 = ?, DEPARTMENT2 = ?, POSTAL = ?, STREET1 = ?,TELL = ?,"
+					+ " FAX = ?, MOBILE = ?, EMAIL = ?, UPDATE_DATE = SYSDATE, CHANGEDATE_DATE = ? "
+					+ "WHERE USER_ID = ?";
+			StringBuilder Sql = new StringBuilder(sql);
+			st = db.connect().prepareStatement(Sql.toString());// 準備//
+			int index = 0;
+			st.setString(++index, form.getFirstName());
+			st.setString(++index, form.getLastName());
+			st.setString(++index, form.getFirstNameKana());
+			st.setString(++index, form.getLastNameKana());
+			st.setString(++index, form.getTitle());
+			st.setString(++index, form.getSex());
+			st.setString(++index, form.getClassification1());
+			st.setString(++index, form.getClassification2());
+			st.setString(++index, form.getPostal());
+			st.setString(++index, form.getStreet1());
+			st.setString(++index, form.getTell());
+			st.setString(++index, form.getFax());
+			st.setString(++index, form.getMobile());
+			st.setString(++index, form.getEmail());
+			st.setString(++index, form.getChangeDate());
+			st.setInt(++index, form.getUserId());
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			db.close(st);
+		}
+
+	}
+
+	@SuppressWarnings("null")
+	public List<UserBean> getOneRecode(int userId) {
+		DBConnector db = new DBConnector();
+		PreparedStatement st = null; // SQLを送るとき必要 /クラス型のst
+		List<UserBean> users = null;
+		ResultSet rs = null;
+		try {
+			st = db.connect().prepareStatement("SELECT FROM CUSTOMER WHERE USER_ID = ?");
+			st.setInt(1, userId);
+
+			users = new ArrayList<>();
+			while (rs.next()) {// 次のレコードに下がれればの条件式//
+				UserBean user = new UserBean();
+				user.setUserId(rs.getInt("USER_ID"));
+				user.setFirstName(rs.getString("FIRST_NAME"));
+				user.setFirstNameKana(rs.getString("FIRST_NAME_KANA"));
+				user.setLastName(rs.getString("LAST_NAME"));
+				user.setLastNameKana(rs.getString("LAST_NAME_KANA"));
+				user.setTitle(rs.getString("TITLE"));
+				user.setSex(rs.getString("SEX"));
+				user.setClassification1(rs.getString("CLASSIFICATION1"));
+				user.setClassification2(rs.getString("CLASSIFICATION2"));
+				user.setCompany(rs.getString("COMPANY"));
+				user.setDepartment1(rs.getString("DEPARTMENT1"));
+				user.setDepartment2(rs.getString("DEPARTMENT2"));
+				user.setPostal(rs.getString("POSTAL"));
+				user.setStreet1(rs.getString("STREET1"));
+				user.setTell(rs.getString("TELL"));
+				user.setMobile(rs.getString("MOBILE"));
+				user.setEmail(rs.getString("EMAIL"));
+				users.add(user);
+			}
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} finally {
+			db.close(st);
+		}
+		return users;
+	}
+
+	//プールダウン用、敬称リスト
+	public List<UserBean> doTitle() {
+		DBConnector db = new DBConnector();
+		List<UserBean> titles = null;
+		PreparedStatement st = null; // SQLを送るとき必要 /クラス型のst
+		ResultSet rs = null;
+		try {
+			titles = new ArrayList<UserBean>();
+			st = db.connect().prepareStatement("SELECT TITLE_NAME FROM TITLE");
+			rs = st.executeQuery();
+			while (rs.next()) {
+				UserBean title = new UserBean();
+				title.setTitleName(rs.getString("TITLE_NAME"));
+				titles.add(title);
+			}
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} // 準備//
+
+		return titles;
+
+	}
 
 }
